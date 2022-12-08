b29212e77edebe98164a4c7a5af55bf6d08c2754
mukhriddin-dev <khodievm@gmail.com>
2022-12-08 18:39:52 +0500

    v1 template starter

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { context } from "./context";
+const App = () => {
+  return (
+    <>
+      <context.Provider value={{}}>
+
+
+      </context.Provider>
+    </>
+  );
+};
+
+export default App;
